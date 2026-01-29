34524fa1548c848f5a8f4a1ebf750554dd202bbe
gee-sr <s.godhashri@gmail.com>
2026-01-29 13:01:36 +0530

    JS functionality for bet submit button

--- assets/js/projectscript.js
+++ assets/js/projectscript.js
@@ -24,7 +24,6 @@ const introOptionsAnswers = {
 
 const section2 = document.getElementById("card2");
 const introResultText = document.getElementById("intro-answer-result");
-console.log(introResultText);
 const defaultResultText = introResultText.innerHTML;
 
 function introBtnClick() {
@@ -53,8 +52,9 @@ function introBtnClick() {
       //   const chosenOptionAllo = document.getElementById("chosen-option-allo");
 
       const optionCorrect = option.classList.contains("correct");
-      console.log(optionCorrect);
-      optionCorrect ? introResultText.innerHTML="<h2>That's right</h2>" : defaultResultText;
+      optionCorrect
+        ? (introResultText.innerHTML = "<h2>That's right</h2>")
+        : defaultResultText;
     });
   });
 
@@ -62,24 +62,60 @@ function introBtnClick() {
   // When any option is clicked, reveal the next section
   // When any option is clicked, change result-text to a or b
   // When any option is clicked, change chosen-option span to option id and get the value of that id of allocation from a pre-made list
-}''
+}
+("");
 
 const section3 = document.getElementById("card3");
-const betskip = document.getElementById("betskip");
-const betsbt = document.getElementById("betsbt");
+const betSkip = document.getElementById("betskip");
+const betSbt = document.getElementById("betsbt");
+const section5 = document.getElementById("card5");
+const betSelector = document.getElementById("betselector");
+const betGraphics = [...document.querySelectorAll(".bet-bar-race")];
+const section4 = document.getElementById("card4");
+const correctSectors = ["Defence", "Agri", "Transport", "UT"];
 
-function betHandling(){
-  betskip.addEventListener("click", () => {
+function betHandling() {
+  betSkip.addEventListener("click", () => {
     section3.style.maxHeight = section3.scrollHeight + "px";
-    console.log(betskip);
-    console.log(betsbt);
-    betskip.style.pointerEvents="none";
-    betsbt.style.pointerEvents="none";
-    betskip.style.backgroundColor="darkgrey";
-    betskip.style.color="white";
-    betsbt.style.backgroundColor="grey";
+    betSkip.style.pointerEvents = "none";
+    betSbt.style.pointerEvents = "none";
+    betSkip.style.backgroundColor = "darkgrey";
+    betSkip.style.color = "white";
+    betSbt.style.backgroundColor = "grey";
+    setTimeout(() => {
+      section5.style.maxHeight = section5.scrollHeight + "px";
+    }, 10000);
   });
-};
+
+  betSbt.addEventListener("click", () => {
+    const betSector = betSelector.value;
+    if (!betSector) {
+      betSbt.textContent = "Please choose sector first!";
+      // return needed to exit the whole function so betSector can get a new value on the next click
+      return;
+    } else {
+      betSbt.textContent = "Good luck!";
+      betSkip.style.pointerEvents = "none";
+      betSbt.style.pointerEvents = "none";
+      betSkip.style.backgroundColor = "darkgrey";
+      betSkip.style.color = "white";
+      betSbt.style.backgroundColor = "grey";
+      setTimeout(() => {
+        section5.style.maxHeight = section5.scrollHeight + "px";
+      }, 10000);
+    }
+
+    betGraphics.forEach((graphic) => {
+      if (graphic.classList.contains(betSector)) {
+        graphic.style.maxHeight = graphic.scrollHeight + "px";
+      }
+    });
+    section4.style.maxHeight = section4.scrollHeight + "px";
+    setTimeout(() => {
+      section5.style.maxHeight = section5.scrollHeight + "px";
+    }, 10000);
+  });
+}
 
 document.addEventListener("DOMContentLoaded", introBtnClick());
-document.addEventListener("DOMContentLoaded",betHandling());
+document.addEventListener("DOMContentLoaded", betHandling());
